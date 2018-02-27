0459a958e5a748ba73689111b0d0ea7914497a2d
Arthinan <arthinankaraket@gmail.com>
2018-02-27 19:59:03 +0700

    commitcode

---
+++ CheckDigit_13/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace CheckDigit_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tx1 = new System.Windows.Forms.TextBox();
+            this.tx2 = new System.Windows.Forms.TextBox();
+            this.tx3 = new System.Windows.Forms.TextBox();
+            this.tx4 = new System.Windows.Forms.TextBox();
+            this.tx5 = new System.Windows.Forms.TextBox();
+            this.tx6 = new System.Windows.Forms.TextBox();
+            this.tx7 = new System.Windows.Forms.TextBox();
+            this.tx8 = new System.Windows.Forms.TextBox();
+            this.tx9 = new System.Windows.Forms.TextBox();
+            this.tx10 = new System.Windows.Forms.TextBox();
+            this.tx11 = new System.Windows.Forms.TextBox();
+            this.tx12 = new System.Windows.Forms.TextBox();
+            this.tx13 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tx1
+            // 
+            this.tx1.Location = new System.Drawing.Point(63, 81);
+            this.tx1.MaxLength = 1;
+            this.tx1.Name = "tx1";
+            this.tx1.Size = new System.Drawing.Size(35, 22);
+            this.tx1.TabIndex = 0;
+            this.tx1.TextChanged += new System.EventHandler(this.Tx1_TextChanged);
+            this.tx1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Tx1_KeyPress);
+            // 
+            // tx2
+            // 
+            this.tx2.Location = new System.Drawing.Point(104, 81);
+            this.tx2.MaxLength = 1;
+            this.tx2.Name = "tx2";
+            this.tx2.Size = new System.Drawing.Size(35, 22);
+            this.tx2.TabIndex = 1;
+            this.tx2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx2_KeyPress);
+            // 
+            // tx3
+            // 
+            this.tx3.Location = new System.Drawing.Point(145, 81);
+            this.tx3.MaxLength = 1;
+            this.tx3.Name = "tx3";
+            this.tx3.Size = new System.Drawing.Size(35, 22);
+            this.tx3.TabIndex = 2;
+            this.tx3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx3_KeyPress);
+            // 
+            // tx4
+            // 
+            this.tx4.Location = new System.Drawing.Point(186, 81);
+            this.tx4.MaxLength = 1;
+            this.tx4.Name = "tx4";
+            this.tx4.Size = new System.Drawing.Size(35, 22);
+            this.tx4.TabIndex = 3;
+            this.tx4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx4_KeyPress);
+            // 
+            // tx5
+            // 
+            this.tx5.Location = new System.Drawing.Point(227, 81);
+            this.tx5.MaxLength = 1;
+            this.tx5.Name = "tx5";
+            this.tx5.Size = new System.Drawing.Size(35, 22);
+            this.tx5.TabIndex = 4;
+            this.tx5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx5_KeyPress);
+            // 
+            // tx6
+            // 
+            this.tx6.Location = new System.Drawing.Point(268, 81);
+            this.tx6.MaxLength = 1;
+            this.tx6.Name = "tx6";
+            this.tx6.Size = new System.Drawing.Size(35, 22);
+            this.tx6.TabIndex = 5;
+            this.tx6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx6_KeyPress);
+            // 
+            // tx7
+            // 
+            this.tx7.Location = new System.Drawing.Point(309, 81);
+            this.tx7.MaxLength = 1;
+            this.tx7.Name = "tx7";
+            this.tx7.Size = new System.Drawing.Size(35, 22);
+            this.tx7.TabIndex = 6;
+            this.tx7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx7_KeyPress);
+            // 
+            // tx8
+            // 
+            this.tx8.Location = new System.Drawing.Point(350, 81);
+            this.tx8.MaxLength = 1;
+            this.tx8.Name = "tx8";
+            this.tx8.Size = new System.Drawing.Size(35, 22);
+            this.tx8.TabIndex = 7;
+            this.tx8.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx8_KeyPress);
+            // 
+            // tx9
+            // 
+            this.tx9.Location = new System.Drawing.Point(391, 81);
+            this.tx9.MaxLength = 1;
+            this.tx9.Name = "tx9";
+            this.tx9.Size = new System.Drawing.Size(35, 22);
+            this.tx9.TabIndex = 8;
+            this.tx9.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx9_KeyPress);
+            // 
+            // tx10
+            // 
+            this.tx10.Location = new System.Drawing.Point(432, 81);
+            this.tx10.MaxLength = 1;
+            this.tx10.Name = "tx10";
+            this.tx10.Size = new System.Drawing.Size(35, 22);
+            this.tx10.TabIndex = 9;
+            this.tx10.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx10_KeyPress);
+            // 
+            // tx11
+            // 
+            this.tx11.Location = new System.Drawing.Point(473, 81);
+            this.tx11.MaxLength = 1;
+            this.tx11.Name = "tx11";
+            this.tx11.Size = new System.Drawing.Size(35, 22);
+            this.tx11.TabIndex = 10;
+            this.tx11.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx11_KeyPress);
+            // 
+            // tx12
+            // 
+            this.tx12.Location = new System.Drawing.Point(514, 81);
+            this.tx12.MaxLength = 1;
+            this.tx12.Name = "tx12";
+            this.tx12.Size = new System.Drawing.Size(35, 22);
+            this.tx12.TabIndex = 11;
+            this.tx12.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Tx12_KeyPress);
+            // 
+            // tx13
+            // 
+            this.tx13.Location = new System.Drawing.Point(555, 81);
+            this.tx13.MaxLength = 1;
+            this.tx13.Name = "tx13";
+            this.tx13.Size = new System.Drawing.Size(35, 22);
+            this.tx13.TabIndex = 12;
+            this.tx13.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Tx13_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(68, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(227, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 65);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "ตรวจสอบความถูกต้อง";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(241, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "กรอกเลขบัตรประชาชน";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(287, 334);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 17);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "เลขที่ถูก";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(246, 369);
+            this.maskedTextBox1.Mask = " 0-0000-00000-00-0 ";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(127, 22);
+            this.maskedTextBox1.TabIndex = 24;
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(525, 423);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 61);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "ล้างข้อมูล";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "รอบแรก";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 17);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "รอบสอง";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(134, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(134, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(134, 217);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 17);
+            this.label8.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 496);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tx13);
+            this.Controls.Add(this.tx12);
+            this.Controls.Add(this.tx11);
+            this.Controls.Add(this.tx10);
+            this.Controls.Add(this.tx9);
+            this.Controls.Add(this.tx8);
+            this.Controls.Add(this.tx7);
+            this.Controls.Add(this.tx6);
+            this.Controls.Add(this.tx5);
+            this.Controls.Add(this.tx4);
+            this.Controls.Add(this.tx3);
+            this.Controls.Add(this.tx2);
+            this.Controls.Add(this.tx1);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Name = "Form1";
+            this.Text = "ตรวจสอบเลขบัตรประชาชน";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tx1;
+        private System.Windows.Forms.TextBox tx2;
+        private System.Windows.Forms.TextBox tx3;
+        private System.Windows.Forms.TextBox tx4;
+        private System.Windows.Forms.TextBox tx5;
+        private System.Windows.Forms.TextBox tx6;
+        private System.Windows.Forms.TextBox tx7;
+        private System.Windows.Forms.TextBox tx8;
+        private System.Windows.Forms.TextBox tx9;
+        private System.Windows.Forms.TextBox tx10;
+        private System.Windows.Forms.TextBox tx11;
+        private System.Windows.Forms.TextBox tx12;
+        private System.Windows.Forms.TextBox tx13;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ CheckDigit_13/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckDigit_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+         
+        private void Tx1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Tx1_KeyPress(object sender, KeyPressEventArgs e ) 
+        {
+            
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx7_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx8_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx9_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx10_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void tx11_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void Tx12_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        private void Tx13_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar) || char.IsControl(e.KeyChar))) //|| (char)('.') == (e.KeyChar)))
+            {
+                e.Handled = true;
+                label1.Text = "**กรุณากรอกเฉพาะตัวเลข 0-9**";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int sum,smod,total, t1, t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13;
+            t1 = int.Parse(tx1.Text);
+            t2 = int.Parse(tx2.Text);
+            t3 = int.Parse(tx3.Text);
+            t4 = int.Parse(tx4.Text);
+            t5 = int.Parse(tx5.Text);
+            t6 = int.Parse(tx6.Text);
+            t7 = int.Parse(tx7.Text);
+            t8 = int.Parse(tx8.Text);
+            t9 = int.Parse(tx9.Text);
+            t10 = int.Parse(tx10.Text);
+            t11 = int.Parse(tx11.Text);
+            t12 = int.Parse(tx12.Text);
+            t13 = int.Parse(tx13.Text);
+            sum = (t1*13) + (t2*12) + (t3*11) + (t4*10) + (t5*9) + (t6*8) + (t7*7) + (t8*6) + (t9*5) + (t10*4) + (t11*3) + (t12*2) ;
+            label6.Text = sum.ToString();
+            smod = sum % 11;
+            label7.Text = smod.ToString();
+            total = 11 - smod;
+            label8.Text = total.ToString();
+            if(total == t13)
+            {
+                label3.Text = "เลขบัตรถูกต้อง";
+            }
+            else
+            {
+                label3.Text = "เลขบัตรไม่ถูกต้อง";
+            }
+            maskedTextBox1.Text = t1.ToString() + t2.ToString() + t3.ToString() + t4.ToString() + t5.ToString() + t6.ToString() + t7.ToString() + t8.ToString() + t9.ToString() + t10.ToString() + t11.ToString() + t12.ToString()+total.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tx1.Text = "";
+            tx2.Text = "";
+            tx3.Text = "";
+            tx4.Text = "";
+            tx5.Text = "";
+            tx6.Text = "";
+            tx7.Text = "";
+            tx8.Text = "";
+            tx9.Text = "";
+            tx10.Text = "";
+            tx11.Text = "";
+            tx12.Text = "";
+            tx13.Text = "";
+            maskedTextBox1.Text = "";
+            label1.Text = "";
+            label3.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+        }
+    }
+}
